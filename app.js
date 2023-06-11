773c322244fb5f22408d7daad67ac3453817cf06
Sabina <sabina@MacBook-Pro-Sabina.local>
2023-06-11 22:20:12 +0400

    PR 12 part 3

--- app.js
+++ app.js
@@ -8,14 +8,6 @@ const app = express();
 
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//   req.user = {
-//     _id: '5648377728fe2577fa37747b6',
-//   };
-
-//   next();
-// });
-
 app.use((req, res, next) => {
   req.user = {
     _id: new mongoose.Types.ObjectId('648377728fe2577fa37747b6'),
