32fd902f077f064c92b68eefb8ffd6e217005c07
Sabina <sabina@MacBook-Pro-Sabina.local>
2023-06-11 22:16:37 +0400

    PR 13 part 2

--- app.js
+++ app.js
@@ -8,20 +8,20 @@ const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '5648377728fe2577fa37747b6',
-  };
-
-  next();
-});
-
 // app.use((req, res, next) => {
 //   req.user = {
-//     _id: new mongoose.Types.ObjectId('648377728fe2577fa37747b6'),
+//     _id: '5648377728fe2577fa37747b6',
 //   };
+
 //   next();
 // });
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: new mongoose.Types.ObjectId('648377728fe2577fa37747b6'),
+  };
+  next();
+});
 
 app.use(router);
 
